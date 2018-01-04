6480c0679e89d1880803e7618a76249d71b7463f
HL <hi.heyse@gmail.com>
2018-01-04 11:14:57 -0500

    Warn users about possible memory leak if createRavenMiddleware called multiple times

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
 const identity = x => x;
 const getUndefined = () => {};
 const filter = () => true;
-function createRavenMiddleware(Raven, options = {}) {
-  // TODO: Validate options.
-  const {
-    breadcrumbDataFromAction = getUndefined,
-    actionTransformer = identity,
-    stateTransformer = identity,
-    breadcrumbCategory = "redux-action",
-    filterBreadcrumbActions = filter,
-    getUserContext
-  } = options;
+const createRavenMiddleware = (function() {
+  let called = false;
+  return (Raven, options = {}) => {
+    // TODO: Validate options.
+    const {
+      breadcrumbDataFromAction = getUndefined,
+      actionTransformer = identity,
+      stateTransformer = identity,
+      breadcrumbCategory = "redux-action",
+      filterBreadcrumbActions = filter,
+      getUserContext
+    } = options;
 
-  return store => {
-    let lastAction;
-
-    Raven.setDataCallback((data, original) => {
-      const state = store.getState();
-      data.extra.lastAction = actionTransformer(lastAction);
-      data.extra.state = stateTransformer(state);
-      if (getUserContext) {
-        data.user = getUserContext(state);
+    if (!called) called = true;
+    else {
+      if (Raven._globalOptions.dataCallback instanceof Function) {
+        console.warn(
+          "Possible memory leak detected! Refer to https://github.com/captbaritone/raven-for-redux."
+        );
       }
-      return original ? original(data) : data;
-    });
+    }
 
-    return next => action => {
-      // Log the action taken to Raven so that we have narrative context in our
-      // error report.
-      if (filterBreadcrumbActions(action)) {
-        Raven.captureBreadcrumb({
-          category: breadcrumbCategory,
-          message: action.type,
-          data: breadcrumbDataFromAction(action)
-        });
-      }
+    return store => {
+      let lastAction;
 
-      lastAction = action;
-      return next(action);
+      Raven.setDataCallback((data, original) => {
+        const state = store.getState();
+        data.extra.lastAction = actionTransformer(lastAction);
+        data.extra.state = stateTransformer(state);
+        if (getUserContext) {
+          data.user = getUserContext(state);
+        }
+        return original ? original(data) : data;
+      });
+
+      return next => action => {
+        // Log the action taken to Raven so that we have narrative context in our
+        // error report.
+        if (filterBreadcrumbActions(action)) {
+          Raven.captureBreadcrumb({
+            category: breadcrumbCategory,
+            message: action.type,
+            data: breadcrumbDataFromAction(action)
+          });
+        }
+
+        lastAction = action;
+        return next(action);
+      };
     };
   };
-}
+})();
 
 module.exports = createRavenMiddleware;
